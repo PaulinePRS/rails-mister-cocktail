73c6b54ada4fa13c3382e32657c7fa4e75dd9868
pauline paris <paulineparisparis@hotmail.fr>
2020-05-23 15:09:31 +0200

    terminé le back

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,30 +1,27 @@
 class DosesController < ApplicationController
-    def new
-    @dose = Dose.new
-  end
+
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    @dose.cocktail = @dose
+    @dose.cocktail = @cocktail
     @dose.save
 
-    redirect_to dose_path(@dose)
+    redirect_to cocktail_path(@cocktail)
 
   end
 
   def destroy
       @dose = Dose.find(params[:id])
       @dose.destroy
-      redirect_to doses_path
+      redirect_to cocktails_path
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description, :ingredient)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 
 end
 
-end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,7 +2,7 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :description, presence: true
-  validates :cocktail, presence:true
-  validates :ingredient, presence:true
-  validates :ingredient, uniqueness: { scope: :dose }
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
+  validates :ingredient, uniqueness: { scope: :description }
 end
